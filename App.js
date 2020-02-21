8760ad6d99b0b54249a4cadf628eb42343d1fa97
Ibrahima Diouf <ibrahimabiram@gmail.com>
2020-02-21 12:49:22 +0000

    unused import

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import "react-native-gesture-handler";
 import React from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 import Home from "./app/views/Home";
 
 export default function App() {
