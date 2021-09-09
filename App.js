60f44138765926786cf553d9ba5ec7215ed0d8da
MamadouSadio <78343283+MamadouSadio@users.noreply.github.com>
2021-09-09 20:13:58 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -7,8 +7,29 @@ import AppLoading from "expo-app-loading";
 import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
 import NoAccountNavigator from "./app/navigation/NoAccountNavigator";
+import * as Sentry from "@sentry/react-native";
+import * as Sentry from 'sentry-expo';
+
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+  dsn: 'YOUR DSN HERE',
+  enableInExpoDevelopment: true,
+  debug: true, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
+});
+
+// Access any @sentry/react-native exports via:
+Sentry.Native.*
+
+// Access any @sentry/browser exports via:
+Sentry.Browser.*
+
+Sentry.init({
+  dsn: "https://a7a5844fe3d446518148480bce6ac2b6@o988745.ingest.sentry.io/5945748",
+});
 
 export default function App() {
+  
   const [user, setUser] = useState();
   const [isReady, setIsReady] = useState(false);
 
@@ -34,3 +55,4 @@ export default function App() {
     </AuthContext.Provider>
   );
 }
+
